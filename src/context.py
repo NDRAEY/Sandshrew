9e8b843988a3ffd719f4a117b782a22f4f510ba5
NDRAEY <andrejpavlenko666@gmail.com>
2023-04-19 19:29:34 +0900

    Add context:clear

--- src/context.py
+++ src/context.py
@@ -2,3 +2,7 @@ class Context:
     def __init__(self, funcs={}, variables={}):
         self.functions = funcs
         self.variables = variables
+
+    def clear(self):
+        self.functions = {}
+        self.variables = {}
